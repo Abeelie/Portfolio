9b1ee842a13bb933c6e0760023a425f01dc31290
Abeelie <abrahaelill@gmail.com>
2021-10-27 18:00:35 -0400

    fonts fix

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Loader = () => {
+    return (
+        <div align="center" style={{marginTop: "120px"}}>
+            <img 
+                src={"https://media0.giphy.com/media/daak2Jqk5NZN2G4PKD/giphy.gif?cid=ecf05e47wj7feev0byhru5efmztm0p4h6oakkbhrv1t24fe0&rid=giphy.gif&ct=g"}
+                alt="loading"
+                width="50%"
+                height="50%"
+            />
+        </div>
+    )
+}
+
+
+export default Loader
